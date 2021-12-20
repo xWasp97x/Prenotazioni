1acd8f1532f9cb4bd9cd8dc39fa704e730b67d8e
wasp97 <imirc97@gmail.com>
2021-12-20 18:23:38 +0100

    Changes for docker container

--- download.py
+++ download.py
@@ -9,50 +9,52 @@ import yaml
 from time import sleep
 import os
 
-now = datetime.now()
-weekday = now.isoweekday()
-monday = now - timedelta(days=weekday - 1)
-friday = monday + timedelta(days=4)
 
-link = 'https://web.spaggiari.eu/home/'
-link_reservations = 'https://web.spaggiari.eu/cvv/app/default/agenda.php?mode=prenotazione&aula_id=8207'
-link_file = f"https://web.spaggiari.eu/cvv/app/default/xml_export.php?stampa=%3Astampa%3A&report_name=&tipo=agenda&data={now.strftime('%d+%m+%Y')}&autore_id=5343519&tipo_export=EVENTI_PRENOTAZIONE&quad=%3Aquad%3A&materia_id=&classe_id=8207&gruppo_id=&ope=RPT&dal={monday.strftime('%Y-%m-%d')}&al={friday.strftime('%Y-%m-%d')}&tipologia=corrente&formato=xml"
+def download():
+    now = datetime.now()
+    weekday = now.isoweekday()
+    monday = now - timedelta(days=weekday - 1)
+    friday = monday + timedelta(days=4)
 
-with open('/config/config.yaml') as file:
-    config = yaml.safe_load(file)
+    link = 'https://web.spaggiari.eu/home/'
+    link_reservations = 'https://web.spaggiari.eu/cvv/app/default/agenda.php?mode=prenotazione&aula_id=8207'
+    link_file = f"https://web.spaggiari.eu/cvv/app/default/xml_export.php?stampa=%3Astampa%3A&report_name=&tipo=agenda&data={now.strftime('%d+%m+%Y')}&autore_id=5343519&tipo_export=EVENTI_PRENOTAZIONE&quad=%3Aquad%3A&materia_id=&classe_id=8207&gruppo_id=&ope=RPT&dal={monday.strftime('%Y-%m-%d')}&al={friday.strftime('%Y-%m-%d')}&tipologia=corrente&formato=xml"
 
-profile = webdriver.FirefoxProfile()
-profile.set_preference('browser.download.folderList', 2)
-profile.set_preference('browser.download.manager.showWhenStarting', False)
-profile.set_preference('browser.download.dir', '/home/wasp97/Downloads')
-profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/vnd.oasis.opendocument.spreadsheet')
-options = Options()
-options.headless = True
+    with open('/config/config.yaml') as file:
+        config = yaml.safe_load(file)
 
-driver = webdriver.Firefox(profile, executable_path=GeckoDriverManager().install(), options=options)
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference('browser.download.folderList', 2)
+    profile.set_preference('browser.download.manager.showWhenStarting', False)
+    profile.set_preference('browser.download.dir', '/home/wasp97/Downloads')
+    profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/vnd.oasis.opendocument.spreadsheet')
+    options = Options()
+    options.headless = True
 
-driver.get(link)
+    driver = webdriver.Firefox(profile, executable_path=GeckoDriverManager().install(), options=options)
 
-id_box = driver.find_element(by=By.ID, value='login')
-id_box.send_keys(config['username'])
+    driver.get(link)
 
-pass_box = driver.find_element(by=By.ID, value='password')
-pass_box.send_keys(config['password'])
+    id_box = driver.find_element(by=By.ID, value='login')
+    id_box.send_keys(config['username'])
 
-'''
-login_button = driver.find_element(by=By.CLASS_NAME, value='accedi btn btn-primary')
-login_button.click()
-'''
+    pass_box = driver.find_element(by=By.ID, value='password')
+    pass_box.send_keys(config['password'])
 
-driver.find_element(By.ID, 'fform').submit()
+    '''
+    login_button = driver.find_element(by=By.CLASS_NAME, value='accedi btn btn-primary')
+    login_button.click()
+    '''
 
-load_check = EC.presence_of_element_located((By.ID, 'data_table'))
-timeout = 10
-WebDriverWait(driver, timeout).until(load_check)
+    driver.find_element(By.ID, 'fform').submit()
 
-driver.set_page_load_timeout(10)
-try:
-    driver.get(link_file)
-except:
-    pass
-driver.quit()
+    load_check = EC.presence_of_element_located((By.ID, 'data_table'))
+    timeout = 10
+    WebDriverWait(driver, timeout).until(load_check)
+
+    driver.set_page_load_timeout(10)
+    try:
+        driver.get(link_file)
+    except:
+        pass
+    driver.quit()
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from math import ceil
 import os
 from datetime import datetime, timedelta
@@ -32,153 +33,164 @@ with open('/config/config.yaml') as file:
 with open(config['surnames_path']) as file:
     surnames = [line.strip() for line in file.readlines()]
 
-'''
+
 user_home = os.path.expanduser('~')
 
 download_folder = os.path.join(user_home, 'Downloads')
-'''
-download_folder = config['download_path']
 
-filename_prefix = datetime.now().strftime('%d%m%Y')
+# download_folder = config['download_path']
 
-files = os.listdir(download_folder)
 
-filename = [f for f in files if re.match(f'export_{filename_prefix}.', f)][0]
+def iteration():
+    download.download()
+    filename_prefix = datetime.now().strftime('%d%m%Y')
 
-print(f'Loading {filename}')
-filepath = os.path.join(download_folder, filename)
+    files = os.listdir(download_folder)
 
-with open(filepath, 'r') as file:
-    lines = file.readlines()
+    filename = [f for f in files if re.match(f'export_{filename_prefix}.', f)][0]
 
-lines = [line.strip() for line in lines]
+    print(f'Loading {filename}')
+    filepath = os.path.join(download_folder, filename)
 
-lines = lines[lines.index('<Row>') + 1:]
+    with open(filepath, 'r') as file:
+        lines = file.readlines()
 
-parsed_lines = []
+    lines = [line.strip() for line in lines]
 
-row = 0
-while row < len(lines):
-    new_line = []
-    while '<Cell>' in lines[row]:
-        line = lines[row]
+    lines = lines[lines.index('<Row>') + 1:]
 
-        line = line.replace('<Cell><Data ss:Type="String">', '')
-        line = line.replace('</Data></Cell>', '')
-        new_line.append(line)
-        row += 1
-    if new_line:
-        parsed_lines.append(new_line)
-    row += 1
+    parsed_lines = []
 
-df = pd.DataFrame(data=parsed_lines[1:], columns=parsed_lines[0])
+    row = 0
+    while row < len(lines):
+        new_line = []
+        while '<Cell>' in lines[row]:
+            line = lines[row]
 
-output_name = os.path.join(config['output_path'], 'reservations.xlsx')
-
-workbook = Workbook()
-sheet = workbook.active
-
-sheet.merge_cells('A1:F1')
-sheet.row_dimensions[1].height = 30
-
-title_text = 'Laboratorio Informatica'
-title_size = 25
-title_width = len(title_text)
-
-title = sheet.cell(row=1, column=1)
-title.value = title_text
-title.alignment = Alignment(horizontal='center', vertical='center')
-
-side = Side(border_style=None)
-no_border = Border(left=side, right=side, top=side, bottom=side)
-
-side = Side(border_style='thin', color='000000')
-boxed = Border(left=side, right=side, top=side, bottom=side)
-title.border = no_border
-title.font = Font(name='Arial', sz=25)
-
-now = datetime.now()
-weekday = now.isoweekday()
-monday = now - timedelta(days=weekday - 1)
-friday = monday + timedelta(days=4)
-
-sheet.merge_cells('A2:F2')
-sheet.row_dimensions[2].height = 30
-
-week_text = f'{monday.strftime("%d/%m/%Y")} - {friday.strftime("%d/%m/%Y")}'
-week_text_size = title_size
-week_text_width = len(week_text)
-
-week_row = sheet.cell(row=2, column=1)
-week_row.value = week_text
-week_row.alignment = Alignment(horizontal='center', vertical='center')
-week_row.border = no_border
-week_row.font = Font(name='Arial', sz=25)
-
-centered = Alignment(horizontal='center', vertical='center')
-
-weekdays = ['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì']
-
-for col, weekday in enumerate(weekdays):
-    cell = sheet.cell(row=3, column=col + 2)
-    cell.value = weekday
-    cell.alignment = centered
-
-bounds = [(timedelta(hours=8, minutes=0), timedelta(hours=8, minutes=50)),
-          (timedelta(hours=8, minutes=50), timedelta(hours=9, minutes=40)),
-          (timedelta(hours=9, minutes=40), timedelta(hours=10, minutes=40)),
-          (timedelta(hours=10, minutes=40), timedelta(hours=11, minutes=30)),
-          (timedelta(hours=11, minutes=30), timedelta(hours=12, minutes=30)),
-          (timedelta(hours=12, minutes=30), timedelta(hours=13, minutes=40)),
-          (timedelta(hours=14, minutes=0), timedelta(hours=15, minutes=0)),
-          (timedelta(hours=15, minutes=0), timedelta(hours=16, minutes=0))]
-
-lessons = ['1 8:00-8:50', '2 8:50-9:40', '3 9:50-10:40', '4 10:40-11:30', '5 11:40-12:30', '6 12:30-13:20',
-           '7 14:10-15:00', '8 15:00-16:00']
-sheet.column_dimensions['A'].width = max([len(lesson) for lesson in lessons])
-
-for row, lesson in enumerate(lessons):
-    cell = sheet.cell(row=row + 4, column=1)
-    cell.value = lesson
-    cell.alignment = centered
-
-for idx, reservation in df.iterrows():
-    weekday = datetime.strptime(reservation['data_inizio'], '%Y-%m-%d').isoweekday()
-    column = weekday + 1
-
-    h, m, _ = map(lambda x: int(x), reservation['ora_inizio'].split(':'))
-
-    start = timedelta(hours=h, minutes=m)
-
-    h, m, _ = map(lambda x: int(x), reservation['ora_fine'].split(':'))
-    end = timedelta(hours=h, minutes=m)
-    length = ceil(((end - start).total_seconds()) / 3600)
-
-    row = 4
-    while not (bounds[row - 4][0] <= start < bounds[row - 4][1]):
+            line = line.replace('<Cell><Data ss:Type="String">', '')
+            line = line.replace('</Data></Cell>', '')
+            new_line.append(line)
+            row += 1
+        if new_line:
+            parsed_lines.append(new_line)
         row += 1
 
-    for i in range(row, row + length):
-        cls = get_class(reservation['utilizzatore'])
-        if reservation['a_nome_di'] == 'DE NICOLA MICAELA MARIA':
-            prof = 'De Nicola'
-        else:
-            prof = get_surname(reservation['a_nome_di'])
-        cell = sheet.cell(i, column)
-        cell.value = f'{cls} ({prof})'.strip()
+    df = pd.DataFrame(data=parsed_lines[1:], columns=parsed_lines[0])
+
+    output_name = os.path.join(config['output_path'], 'reservations.xlsx')
+
+    workbook = Workbook()
+    sheet = workbook.active
+
+    sheet.merge_cells('A1:F1')
+    sheet.row_dimensions[1].height = 30
+
+    title_text = 'Laboratorio Informatica'
+    title_size = 25
+    title_width = len(title_text)
+
+    title = sheet.cell(row=1, column=1)
+    title.value = title_text
+    title.alignment = Alignment(horizontal='center', vertical='center')
+
+    side = Side(border_style=None)
+    no_border = Border(left=side, right=side, top=side, bottom=side)
+
+    side = Side(border_style='thin', color='000000')
+    boxed = Border(left=side, right=side, top=side, bottom=side)
+    title.border = no_border
+    title.font = Font(name='Arial', sz=25)
+
+    now = datetime.now()
+    weekday = now.isoweekday()
+    monday = now - timedelta(days=weekday - 1)
+    friday = monday + timedelta(days=4)
+
+    sheet.merge_cells('A2:F2')
+    sheet.row_dimensions[2].height = 30
+
+    week_text = f'{monday.strftime("%d/%m/%Y")} - {friday.strftime("%d/%m/%Y")}'
+    week_text_size = title_size
+    week_text_width = len(week_text)
+
+    week_row = sheet.cell(row=2, column=1)
+    week_row.value = week_text
+    week_row.alignment = Alignment(horizontal='center', vertical='center')
+    week_row.border = no_border
+    week_row.font = Font(name='Arial', sz=25)
+
+    centered = Alignment(horizontal='center', vertical='center')
+
+    weekdays = ['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì']
+
+    for col, weekday in enumerate(weekdays):
+        cell = sheet.cell(row=3, column=col + 2)
+        cell.value = weekday
         cell.alignment = centered
 
-for col in range(6):
-    max_length = max([len(sheet.cell(row, 6).value) for row in range(3, 9, 1) if sheet.cell(row, 6).value is not None])
-    sheet.column_dimensions[chr(ord('A') + col)].width = max_length
+    bounds = [(timedelta(hours=8, minutes=0), timedelta(hours=8, minutes=50)),
+              (timedelta(hours=8, minutes=50), timedelta(hours=9, minutes=40)),
+              (timedelta(hours=9, minutes=40), timedelta(hours=10, minutes=40)),
+              (timedelta(hours=10, minutes=40), timedelta(hours=11, minutes=30)),
+              (timedelta(hours=11, minutes=30), timedelta(hours=12, minutes=30)),
+              (timedelta(hours=12, minutes=30), timedelta(hours=13, minutes=40)),
+              (timedelta(hours=14, minutes=0), timedelta(hours=15, minutes=0)),
+              (timedelta(hours=15, minutes=0), timedelta(hours=16, minutes=0))]
 
-for row in range(3, 12, 1):
-    for col in range(1, 7):
-        cell = sheet.cell(row, col)
-        cell.border = boxed
+    lessons = ['1 8:00-8:50', '2 8:50-9:40', '3 9:50-10:40', '4 10:40-11:30', '5 11:40-12:30', '6 12:30-13:20',
+               '7 14:10-15:00', '8 15:00-16:00']
+    sheet.column_dimensions['A'].width = max([len(lesson) for lesson in lessons])
 
-Worksheet.set_printer_settings(sheet, orientation='landscape', paper_size='9')
-sheet.print_options.horizontalCentered = True
-sheet.print_options.verticalCentered = True
+    for row, lesson in enumerate(lessons):
+        cell = sheet.cell(row=row + 4, column=1)
+        cell.value = lesson
+        cell.alignment = centered
 
-workbook.save(output_name)
+    for idx, reservation in df.iterrows():
+        weekday = datetime.strptime(reservation['data_inizio'], '%Y-%m-%d').isoweekday()
+        column = weekday + 1
+
+        h, m, _ = map(lambda x: int(x), reservation['ora_inizio'].split(':'))
+
+        start = timedelta(hours=h, minutes=m)
+
+        h, m, _ = map(lambda x: int(x), reservation['ora_fine'].split(':'))
+        end = timedelta(hours=h, minutes=m)
+        length = ceil(((end - start).total_seconds()) / 3600)
+
+        row = 4
+        while not (bounds[row - 4][0] <= start < bounds[row - 4][1]):
+            row += 1
+
+        for i in range(row, row + length):
+            cls = get_class(reservation['utilizzatore'])
+            if reservation['a_nome_di'] == 'DE NICOLA MICAELA MARIA':
+                prof = 'De Nicola'
+            else:
+                prof = get_surname(reservation['a_nome_di'])
+            cell = sheet.cell(i, column)
+            cell.value = f'{cls} ({prof})'.strip()
+            cell.alignment = centered
+
+    for col in range(6):
+        max_length = max([len(sheet.cell(row, 6).value) for row in range(3, 9, 1) if sheet.cell(row, 6).value is not None])
+        sheet.column_dimensions[chr(ord('A') + col)].width = max_length
+
+    for row in range(3, 12, 1):
+        for col in range(1, 7):
+            cell = sheet.cell(row, col)
+            cell.border = boxed
+
+    Worksheet.set_printer_settings(sheet, orientation='landscape', paper_size='9')
+    sheet.print_options.horizontalCentered = True
+    sheet.print_options.verticalCentered = True
+
+    workbook.save(output_name)
+
+
+if __name__ == '__main__':
+    delay = config['loop_delay']
+    while True:
+        iteration()
+        time.sleep(delay)
+
